69cae8ac9dc11fddb39730aa9ac02af3014f9d7c
FatmaBeyza <gungorrbeyza@gmail.com>
2020-07-01 00:03:35 +0300

    server eklendi

--- indexed-capitalization.js
+++ indexed-capitalization.js
@@ -1,9 +1,14 @@
-// string işlemler uppercase 
-
+// server eklendi
 var str = "abcdef";
+var http = require('http');
 
 function toUpper(str){
     return str[0] + str[1].toUpperCase() + str[2].toUpperCase() + str[3] + str[4] + str[5].toUpperCase()
 }
 console.log(toUpper(str))
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(toUpper(str));
+  }).listen(8000);
 
