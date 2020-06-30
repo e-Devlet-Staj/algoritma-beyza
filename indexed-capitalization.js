c3382abb6d99f77a81db886ddd094fc0284be2b0
FatmaBeyza <gungorrbeyza@gmail.com>
2020-07-01 01:10:02 +0300

    capitalize fonksiyonu düzenlendi

--- indexed-capitalization.js
+++ indexed-capitalization.js
@@ -1,14 +1,37 @@
-// server eklendi
-var str = "abcdef";
+// capitalize function 
+var str = "";
 var http = require('http');
+var arr = []; 
+var output = ""
 
-function toUpper(str){
-    return str[0] + str[1].toUpperCase() + str[2].toUpperCase() + str[3] + str[4] + str[5].toUpperCase()
+function capitalize(str, arr){
+var x = str.length;   
+var i= 0;
+
+for(i; i<x; i++){ 
+   if (i== 0)
+     output += str.charAt(0)
+   else if (i==1)
+    output += str.charAt(1).toUpperCase()     
+   else if (i==2)
+    output += str.charAt(2).toUpperCase()
+   else if (i==3)
+    output += str.charAt(3)
+   else if (i==4)
+    output += str.charAt(4)
+   else if (i==5)
+   output += str.charAt(5).toUpperCase()
+   else
+   output+= " "     
 }
-console.log(toUpper(str))
+return output
+}
+
+capitalize("abcdef",[1,2,5])  //aBCdeF
+//capitalize("abcdef",[1,2,5,100]) //aBCdeF
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(toUpper(str));
-  }).listen(8000);
-
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(output); 
+  console.log("listening to server...")
+}).listen(8080);
