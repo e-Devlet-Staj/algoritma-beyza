17f89f9e0c0f7a2329766ab3840567d4e2c31c25
FatmaBeyza <gungorrbeyza@gmail.com>
2020-07-01 11:03:04 +0300

    güncelleme

--- indexed-capitalization.js
+++ indexed-capitalization.js
@@ -1,18 +1,27 @@
-// capitalize function / yeniden düzenleme
+// capitalize function / yeniden düzenleme // 
 
 var str = "";
 var arr = []; 
 var output = ""
 
 function capitalize(str, arr){
-var x = arr.length;   
-var i= 0;
+var x = arr.length;
+var y = str.length;    
+var i,j= 0;
 
-for(i; i<x; i++){ 
-  var a = str.charAt(arr[i]).toUpperCase()  
-  //console.log(a)      
-  output+=a
+for (j;j<y;j++){
+  if (!(arr.includes(j))){
+    var a = str.charAt(j);
+    output+=a
+  }
+  else if (arr.includes(j)){ 
+    var b = str.charAt(j).toUpperCase()  
+    output+=b
+  }
+  else
+      output += " "   
 }
+
 console.log(output)
 output=""
 return output
@@ -22,3 +31,5 @@ capitalize("abcdef",[1,2,5])  //aBCdeF
 capitalize("abcdef",[1,2,5,100]) //aBCdeF
 capitalize("xyzt",[0,3]) // Xyzt
 capitalize("abcdef",[0,3,4]) // AbcDEf
+capitalize("merhaba",[1,2,9]) // mERhaba
+capitalize("abcdefxyztyz",[0,1,3,100]) // ABcDefxyztyz
