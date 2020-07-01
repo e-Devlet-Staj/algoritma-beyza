da943954497064bde52e218ae80d547c5a4dab46
FatmaBeyza <gungorrbeyza@gmail.com>
2020-07-01 20:25:24 +0300

    if sartı kaldırılarak yeniden düzenlendi

--- encrypt-this.js
+++ encrypt-this.js
@@ -1,17 +1,7 @@
 function encryptThis(str){ 
-    var newString = ""    
-
-    if (!(str.includes(" "))){  // verilen str tek kelime, icinde bosluk yoksa
-    var f = str.charCodeAt(0)  // ilk elemanın ascii kodu
-      newString += f
-        for(var i=str.length-1; i>0 ; i--){  // kalanı ters cevir
-            newString += str[i];        
-        }    
-    }
-
-    else if (str.includes(" ")){  // verilen kelime icinde bosluk varsa
-        var newreverse="";
-        var parts = str.split(" ");  // kelimeleri ayrı incelemek icin
+    var newString = ""; 
+    var newreverse = "";
+    var parts = str.split(" "); // birden fazla kelime olabildigi icin
         for ( var i = 0; i < parts.length; i++ ){        
             var x = parts[i].charCodeAt(0); 
             var y = parts[i].substr(1);
@@ -19,13 +9,13 @@ function encryptThis(str){
                 newreverse += y[j];        
             }            
             parts[i] = x + newreverse;  
-            newreverse=""           
+            newreverse="";           
             var joinle = parts.join(" ");   
-        }         
-    }              
+        }       
     return newString
 }
 encryptThis("Hello")
 encryptThis("good") 
 encryptThis("hello world") 
 encryptThis("abc def ghi xyz")
+encryptThis("merhaba dünya")
