f1f0c89e1dd845f79e333459eaf5d08ae438f7b8
FatmaBeyza <gungorrbeyza@gmail.com>
2020-07-02 22:52:09 +0300

    issue'lar düzenlendi

--- decrypt-this.js
+++ decrypt-this.js
@@ -1,30 +1,27 @@
-function decryptThis(newStr){
+function decryptThis(newStr) {
     var newString = "";  
     var parts = newStr.split(" ");   
 
-    for(var i = 0; i< parts.length; i++){
+    for(var i = 0; i < parts.length; i++) {
         var regex = /^\d{1,3}/;  // string basındaki 1-3 karakterli sayılar
-        var match = regex.exec(parts[i]); // string icinde regex ara
-        if(match){  // eger aranan ifade yer alıyorsa
+        var match = regex.exec(parts[i]);
+        
+        if(match){
             var matched = match[0];        
-            var ascii_char = String.fromCharCode(matched);  // ascii kod -> char
-            var newss = parts[i].replace(matched,ascii_char);                 
-            
-                for(var j=newss.length-1; j>0 ; j--){
-                    newString += newss[j];        
+            var asciiChar = String.fromCharCode(matched);  // ascii kod -> char
+            var strReplaced = parts[i].replace(matched, asciiChar);
+
+                for(var j = strReplaced.length - 1; j > 0; j--) {
+                    newString += strReplaced[j];        
                 } 
-            
-            parts[i] = ascii_char + newString;            
-            newString="";         
+
+            parts[i] = asciiChar + newString;            
+            newString = "";         
             var join_s = parts.join(" ");                      
-        } else {
-            // aranan ifadenin string icerisinde yer almama durumu
-            // sifreli degil
-        } 
+        } else {} 
     }    
     return (join_s)
 }
-
 decryptThis("72olle")
 decryptThis("103doo") 
 decryptThis("104olle 119dlro") 
