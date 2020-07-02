a8417e90b4fad78e471062e73f95046de0385bed
FatmaBeyza <gungorrbeyza@gmail.com>
2020-07-02 12:29:13 +0300

    düzenleme yapıldı

--- encrypt-this.js
+++ encrypt-this.js
@@ -1,18 +1,17 @@
-function encryptThis(str){ 
-    var newString = ""; 
+function encryptThis(str){     
     var newreverse = "";
     var parts = str.split(" "); // birden fazla kelime olabildigi icin
         for ( var i = 0; i < parts.length; i++ ){        
-            var x = parts[i].charCodeAt(0); 
-            var y = parts[i].substr(1);
-            for(var j=y.length-1; j>=0 ; j--){
+            var x = parts[i].charCodeAt(0); // ilk karakterin ascii kodunu al
+            var y = parts[i].substr(1);  // kalan kısmı y'ye ata
+            for(var j=y.length-1; j>=0 ; j--){  // tersine cevir
                 newreverse += y[j];        
             }            
-            parts[i] = x + newreverse;  
+            parts[i] = x + newreverse;  // ascii kodu + ters sifre  
             newreverse="";           
-            var joinle = parts.join(" ");   
-        }       
-    return newString
+            var joinle = parts.join(" ");  // birlestir - birden fazla kelime durumu 
+        }        
+    return joinle
 }
 encryptThis("Hello")
 encryptThis("good") 
