92fc627e8f1f23905c2ed9868386be22b3657b11
FatmaBeyza <gungorrbeyza@gmail.com>
2020-07-02 22:42:51 +0300

    issue'lar düzenlendi

--- indexed-capitalization.js
+++ indexed-capitalization.js
@@ -1,30 +1,22 @@
-var str = "";
-var arr = []; 
-var output = "";
-
-function capitalize(str, arr){
-  var y = str.length;  
-
-  for (var j=0;j<y;j++){
-    if (!(arr.includes(j))){
+function capitalize(str, arr) {
+  var output = "";
+  
+  for (var j = 0; j < str.length; j++) {
+    if (!(arr.includes(j))) {
       var a = str.charAt(j);
-      output+=a;
-    }
-    else if (arr.includes(j)){ 
+      output += a;
+    } else if (arr.includes(j)) {
       var b = str.charAt(j).toUpperCase();  
-      output+=b;
-    }
-    else {
-        output += " ";
+      output += b;
+    } else {
+      output += " ";
     }  
   } 
-  output="";
   return output;
 }
-
 capitalize("abcdef",[1,2,5])  //aBCdeF
-capitalize("abcdef",[1,2,5,100]) //aBCdeF
-capitalize("xyzt",[0,3]) // Xyzt
-capitalize("abcdef",[0,3,4]) // AbcDEf
-capitalize("merhaba",[1,2,9]) // mERhaba
-capitalize("abcdefxyztyz",[0,1,3,100]) // ABcDefxyztyz
+capitalize("abcdef",[1,2,5,100])  // aBCdeF
+capitalize("xyzt",[0,3])  // Xyzt
+capitalize("abcdef",[0,3,4])  // AbcDEf
+capitalize("merhaba",[1,2,9])  // mERhaba
+capitalize("abcdefxyztyz",[0,1,3,100])  // ABcDefxyztyz
