1ce6d9b9811b3056b1e8422e2759fcfe6b7f8c37
FatmaBeyza <gungorrbeyza@gmail.com>
2020-07-03 12:20:11 +0300

    düzenleme yapıldı

--- decrypt-this.js
+++ decrypt-this.js
@@ -1,27 +1,26 @@
 function decryptThis(newStr) {
-    var newString = "";  
-    var parts = newStr.split(" ");   
+    var parts = newStr.split(" ");
 
     for(var i = 0; i < parts.length; i++) {
+        var newString = "";
         var regex = /^\d{1,3}/;  // string basındaki 1-3 karakterli sayılar
         var match = regex.exec(parts[i]);
         
         if(match){
-            var matched = match[0];        
+            var matched = match[0];
             var asciiChar = String.fromCharCode(matched);  // ascii kod -> char
             var strReplaced = parts[i].replace(matched, asciiChar);
 
-                for(var j = strReplaced.length-1; j > 0; j--) {
-                    newString += strReplaced[j];        
-                } 
+            for(var j = strReplaced.length-1; j > 0; j--) {
+                newString += strReplaced[j];
+            } 
 
-            parts[i] = asciiChar + newString;            
-            newString = "";         
-            var join_s = parts.join(" ");                      
+            parts[i] = asciiChar + newString;
+            var joinStr = parts.join(" ");
         } else {} 
     }    
-    return (join_s)
+    return (joinStr)
 }
 decryptThis("72olle")
 decryptThis("103doo") 
-decryptThis("104olle 119dlro") 
+decryptThis("104olle 119dlro")
